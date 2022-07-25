3f80e818d021a13d3dd993b49c51dbdea803b5a0
TheFractalCat <tc@fractalCat.io>
2022-07-24 22:08:36 -0400

    TLC Node setup

---
+++ nodes.py
@@ -0,0 +1,20 @@
+"""
+This modeule defines the basic structure and function of a TLC Node - the basic form of storage used by the TLCVM
+"""
+
+
+
+class	TLCNode:
+	"""
+	the basic node used as a parent for all other Node types. it provides default behavior used by other types
+	"""
+
+	def	__init__(self):
+		"""
+		constructor for the Node base class - currently it does nothing
+		"""
+		pass
+
+
+
+
