66673fdfc4607e67f543b6602e5df5264ed75e85
TheFractalCat <tc@fractalCat.io>
2022-07-27 22:45:36 -0400

    TLC Dict setup

--- includes/TLCdict.py
+++ includes/TLCdict.py
@@ -21,3 +21,35 @@ class	TLCDict:
 		self._values = list()
 
 
+
+
+
+	def	addEntry(self, key, value):
+		"""
+		adds an entry to the TLCDict and returns the dictionary index
+		"""
+		self._values.append(value)
+		index = len(self._values)-1
+		self._names[key.casefold()] = index
+
+		return	index
+
+
+
+
+
+	def	getEntry(self, target):
+		"""
+		looks up a dictionary entry and returns a tuple of the form (index, value)
+		returns (None, None) if no match is found
+		"""
+		target = target.casefold()
+
+		if	target in self._names:
+			index = self._names[target]
+			value = self._values[index]
+		else:
+			index = None
+			value = None
+
+		return	(index, value)
