b8bb0f8fa79351440513480d638e4ac728f6c65e
TheFractalCat <tc@fractalCat.io>
2022-08-21 15:07:35 -0400

    tlc processor definition

---
+++ _includes/components/processor.py
@@ -0,0 +1,27 @@
+"""
+this module defines the basic TLC processor used by the tlcVM
+"""
+
+
+
+#	--------------
+#	imports needed
+#	--------------
+import	.stack
+
+
+#	-----------
+#	export list
+#	-----------
+__all__ = [
+			'TLCException',
+		]
+
+
+
+
+
+class TLCProcessor:
+	"""
+	the virtual processor used by the TLC
+	"""
